c3993650ded572ff2140b4c6fb9fccdaaf26531c
Faduka <playvanok@gmail.com>
2024-07-21 06:47:26 +0300

    Updated header, updated dropdown etc.

---
+++ src/main/resources/static/js/dropdown-animation.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach(dropdown => {
+        dropdown.addEventListener('show.bs.dropdown', function () {
+            this.querySelector('.dropdown-arrow').style.transform = 'rotate(90deg)';
+        });
+
+        dropdown.addEventListener('hide.bs.dropdown', function () {
+            this.querySelector('.dropdown-arrow').style.transform = 'rotate(0deg)';
+        });
+    });
+});
